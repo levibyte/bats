77f7a8613f8ebbfba979a23e63dc8ca7f1338d45
Levibyte ( libxelar.so ) <levibyte@users.noreply.github.com>
2019-03-05 17:52:27 +0400

    Update common.bash

--- special_tests/common.bash
+++ special_tests/common.bash
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-#mkdir -p  dir
-#mkdir -p  not_exisiting_dir
-#touch not_writable_file
-#chmod 555 not_writable_file
-#mkdir -p  dir
-#mkdir -p  not_writable_dir
-#chmod 555 not_writable_dir/
+mkdir -p  dir
+mkdir -p  not_exisiting_dir
+touch not_writable_file -f
+chmod 555 not_writable_file -f
+mkdir -p  dir
+mkdir -p  not_writable_dir
+chmod 555 not_writable_dir/ -f
 
+if [ -f .count ]; then
+chmod 777 .count
+fi
+
+rm -rf .count
 #chmod 777 .count -f
-#touch .count -f
+touch .count -f
 #fix unstabillity
-#chmod 777 .count -f
-#echo 0 > .count
-#chmod 555 .count -f
+chmod 777 .count -f
+echo 0 > .count
+chmod 555 .count -f
+
