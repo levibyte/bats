03d094f21323520cc77788c32f11727dae1f7d25
Levibyte ( libxelar.so ) <levibyte@users.noreply.github.com>
2019-03-05 17:57:56 +0400

    Update common.bash

--- special_tests/common.bash
+++ special_tests/common.bash
@@ -3,20 +3,21 @@
 mkdir -p  dir
 mkdir -p  not_exisiting_dir
 touch not_writable_file -f
-chmod 555 not_writable_file -f
+chmod 555 not_writable_file 
 mkdir -p  dir
 mkdir -p  not_writable_dir
-chmod 555 not_writable_dir/ -f
+chmod 555 not_writable_dir/ 
 
 if [ -f .count ]; then
-chmod 777 .count
+  chmod 777 .count
+  rm -rf .count
 fi
 
-rm -rf .count
 #chmod 777 .count -f
 touch .count -f
 #fix unstabillity
-chmod 777 .count -f
+chmod 777 .count 
 echo 0 > .count
-chmod 555 .count -f
+chmod 555 .count
+
 
